98e977cfebb7773e817a1fef84f2069c6a43e463
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-03-06 16:42:00 +0100

    renames module

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name="owl_augmentator",
+    name="owlready2_augmentator",
     version="0.1",
     description="Augmentator library for owlready2",
     author ="Lukas Westhofen",
     author_email="lukas.westhofen@dlr.de",
-    py_modules=["owl_augmentator"],
+    py_modules=["owlready2_augmentator"],
     install_requires=["owlready2"]
 )
