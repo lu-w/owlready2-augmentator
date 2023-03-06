22044ba2d441ff0a06bf3e232ff0352ac358de35
Lukas Westhofen <lukas.westhofen@offis.de>
2023-03-06 14:57:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     description="Augmentator library for OWLReady2",
     author ="Lukas Westhofen",
-    author_email="lukas.westhofen@offis.de",
+    author_email="lukas.westhofen@dlr.de",
     py_modules=["owl_augmentator"],
     install_requires=["owlready2"]
     )
